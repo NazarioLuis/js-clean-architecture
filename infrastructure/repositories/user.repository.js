cb83a071b6eca189de731aa3f5bdc8bd9b1a686f
NazarioLuis <nazarioluisaf@gmail.com>
2021-06-30 11:52:35 -0400

    unit testing using Mocha

--- infrastructure/repositories/user.repository.js
+++ infrastructure/repositories/user.repository.js
@@ -1,16 +1,41 @@
 const UserBehavior = require("../../domain/behavior/user.behavior")
 
 class UserRepository extends UserBehavior{
-    getAll = () => 'fake getAll'
-    get = id => `fake get ${id}`
-    create = entity => entity
-    update = (entity,id) => {
-        return {
-            id,
-            data: entity,
-        }
+    users = [{
+        id:1,
+        firstname:"Eddard",
+        lastname:"Stark",
+        nick:"ned",
+        pass:"123",
+    },{
+        id:2,
+        firstname:"Catelyn",
+        lastname:"Tully",
+        nick:"cat",
+        pass:"123",
+    }]
+    getAll = () => this.users
+    get = id => this.users.find(x => x.id == id)
+    create = entity => {
+        this.users.push({
+            ...entity,
+            id:this.users[this.users.length-1].id+1,
+        })
+        return this.users[this.users.length-1]
     }
-    delete = id => `fake get ${id}`
+    update = (entity,id) => {
+        const index = this.users.findIndex(x => x.id == id)
+        this.users[index]={
+            ...entity,
+            id:this.users[index].id,
+        }
+        return this.users[index]
+    }
+    delete = id => {
+        const index = this.users.findIndex(x => x.id == id)
+        this.users.splice(index, 1);
+        return id
+    }
 }
 
 module.exports = UserRepository
